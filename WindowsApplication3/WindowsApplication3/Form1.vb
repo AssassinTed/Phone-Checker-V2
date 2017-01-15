2828bfd4e0f2e0fd72cb4ec74fb88784a04c4f89
AssassinTed <theodorfbi@gmail.com>
2017-01-15 16:20:09 +0200

    Add files via upload

---
+++ WindowsApplication3/WindowsApplication3/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Not IsNumeric(TextBox1.Text) Then
+            MsgBox("Type only numbers")
+        ElseIf Not TextBox1.Text.StartsWith("69") Then
+            MsgBox("Should start with '69' ")
+        Else
+            MsgBox("The phone number is correct")
+
+        End If
+    End Sub
+End Class
